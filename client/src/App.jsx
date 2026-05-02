a77d84905118bdf0ba9799de42dcced9c14784ed
Sandeep <sandeepsamkenda124@gmail.com>
2026-05-02 16:13:33 +0530

    Basic Page Struc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import ProductCard from './components/productCard'
+import HomePage from './components/homePage'
+import AdminPage from './components/adminPage'
 
 function App() {
  
   return (
-    <>
-      <div>
-        <ProductCard name="Rain Forest" description="Explore the lush greenery and diverse wildlife of the rain forest." imageUrl="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg" />
-        <ProductCard name="Mountain Trekking" description="Embark on an adventure through scenic mountain trails and breathtaking views." imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6ub0pgXnjiEfXzH4seCasYRX8VtHsDFBLDQ&s" />
-        <ProductCard name="Beach Paradise" description="Relax on pristine beaches and enjoy crystal-clear waters." imageUrl="https://img.freepik.com/free-photo/beach-with-coconut-tree-sunset_268835-4049.jpg?semt=ais_hybrid&w=740&q=80" />
+
+      <div className= 'w-[700px] h-[700px] border-[6px] flex items-center justify-center'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/admin' element={<AdminPage />} />
+        </Routes>
       </div>
-    </>
+
   )
 }
 
---
+++ client/src/components/adminPage.jsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <h1 className='text-4xl font-bold'>Admin Page</h1>
+    </div>
+  )
+}
